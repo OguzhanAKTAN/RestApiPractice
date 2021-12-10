3d5f9267c6d1e7a6dfacab766458cfc43b1381a3
aktan2778 <66529142+aktan2778@users.noreply.github.com>
2021-12-10 16:43:04 +0300

    added user router

---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+var fs = require('fs')
+
+router.get('/', (req,res) => {
+    fs.readFile(__dirname+"/"+"users.json","utf-8",(err,data) => {
+        console.log(data);
+        res.end(data);
+    })
+})
+
+router.get('/:id', (req, res) => {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', (err, data) => {
+       var users = JSON.parse( data );
+       var user = users["user" + req.params.id] 
+       console.log( user );
+       res.end( JSON.stringify(user));
+    });
+ })
+
+ module.exports = router 
--- server.js
+++ server.js
@@ -2,23 +2,8 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
-app.get('/listUsers', function(req,res){
-    fs.readFile(__dirname+"/"+"users.json","utf-8",function(err,data){
-        console.log(data);
-        res.end(data);
-    })
-})
-
-app.get('/:id', function (req, res) {
-    // First read existing users.
-    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       var users = JSON.parse( data );
-       var user = users["user" + req.params.id] 
-       console.log( user );
-       res.end( JSON.stringify(user));
-    });
- })
-
+const userRouter = require('./routes/users')
+app.use('/users',userRouter)
 var userToBeAdded = {
     "user4":{
         "name":"Oguzhan",
